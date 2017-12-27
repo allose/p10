e8efbb3c62c6dfbde25e852120fbc1b6ac3e3daf
allose <allose94@gmail.com>
2017-12-27 18:29:22 +0100

    Aplicacion dibuja triangulos

--- dibujaFiguras.cpp
+++ dibujaFiguras.cpp
@@ -30,7 +30,30 @@ void cuadrado(int al,int anc, char simb,bool rel)
     }
 }
 
-
+void triangulo(int anc, char simb,bool rel)
+{
+    if (anc==1)
+        anc+=2;
+    int principio=(anc-1)/2;
+    int fin=(anc-1)/2;
+    for (int i=1; i<=(anc-1)/2; i++)
+    {
+        for (int j=0; j<anc;j++)
+        {
+            if (j==principio || j==fin)
+                cout << simb;
+            else if (j>=principio && j<=fin && rel)
+                cout << simb;
+            else if (i==(anc-1)/2 && j>=principio && j<=fin)
+                cout << simb;
+            else
+                cout << " ";
+        }
+        principio--;
+        fin++;
+        cout << endl;
+    }
+}
 
 int main ()
 {
@@ -41,27 +64,34 @@ int main ()
     cout << "-----------------------" << endl;
     cout << "CONSTRUYAMOS UNA FIGURA" << endl;
     cout << "-----------------------" << endl << endl;
-
-
+    do {
+        cout << "Que figura quieres triangulo o cuadrado? (t/c)" << endl;
+        cin >> fig;
+        cout << endl;
+    }while(fig!='t' && fig!='T' && fig!='c' && fig!='C');
+        cin.get();
     do{
-
+        if (fig=='t' || fig=='T' )
+            cout << "La anchura tiene que ser un número impar" << endl;
+        
         cout << "Ingrese una anchura entre 1 y 50: " << endl;
         getline(cin,numero);
         anc=atoi(numero.c_str());
         
-    }while(anc<1 || anc>50 );
+    }while(anc<1 || anc>50 || (anc%2==0 && fig!='c' && fig!='C'));
     
+    if (fig=='c' || fig=='C'){
         do{
             cout << "Ingrese una altura entre 1 y 50: " << endl;
             getline(cin,numero);
             al=atoi(numero.c_str());
         }while(al<1 || al>50 );
+    }
     
     do{
         cout << "Quieres que la figura este rellena con simbolos? (S/N): " << endl;
         cin >> relleno;
     }while(relleno!='S' && relleno!='N' && relleno!='s' && relleno!='n');
-    
     if (relleno=='S' || relleno=='s')
         rel=true;
     
@@ -69,6 +99,9 @@ int main ()
     cin >> simb;
     cout << endl;
     
-
+    if (fig=='c' || fig=='C')
         cuadrado(al,anc,simb,rel);
+    else if (fig=='t' || fig=='T')
+        triangulo(anc,simb,rel);
+
 }
